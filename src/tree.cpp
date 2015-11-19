52a5651787c4ce57935e80f075644d1995960e98
alevy <levyakane@gmail.com>
2015-11-19 18:04:38 +0100

    tree file added

---
+++ src/tree.cpp
@@ -0,0 +1,6 @@
+#include "tree.hpp"
+
+Tree::Tree(ofVec3f zoneDim):
+    _zoneDim(zoneDim){
+}
+
---
+++ src/tree.hpp
@@ -0,0 +1,18 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "ofMain.h"
+#include "view.hpp"
+
+
+class Tree
+{
+public:
+    Tree(ofVec3f zoneDim);
+
+
+private:
+    ofVec3f _zoneDim;
+};
+
+#endif //TREE_H
